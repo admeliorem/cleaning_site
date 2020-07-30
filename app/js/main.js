5e3650b32b4d1ffce09715e8b27ccc834824d605
admeliorem <vinnipuh545@gmail.com>
2020-07-30 22:35:20 +0300

    add project

---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('.product__adv').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        prevArrow: '<div class="prev"></div>',
+        nextArrow: '<div class="next"></div>',
+    });
+});
