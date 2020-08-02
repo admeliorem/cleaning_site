d3fb1439bb11ebc2c00b2b7ac9453f1e35b9f404
admeliorem <vinnipuh545@gmail.com>
2020-08-02 18:58:15 +0300

    fix bugs

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,11 @@ $(function () {
         prevArrow: '<div class="prev"></div>',
         nextArrow: '<div class="next"></div>',
     });
+    $('.rewivs__items').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<div class="prev review"></div>',
+        nextArrow: '<div class="next review"></div>',
+        });
 });
